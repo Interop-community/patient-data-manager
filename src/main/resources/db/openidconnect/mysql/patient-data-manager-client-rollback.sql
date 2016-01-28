1a95b5d882b744ff4ddc9a25cd5942acf4490e63
Travis Cummings <travis@istalusconsulting.com>
2016-01-28 15:47:59 -0700

    Refactored hsp-reference-apps to be hspc-reference-apps

---
+++ src/main/resources/db/openidconnect/mysql/patient-data-manager-client-rollback.sql
@@ -0,0 +1,14 @@
+USE oic;
+
+SET AUTOCOMMIT = 0;
+
+START TRANSACTION;
+
+-- Patient Data Manager App
+DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'patient_data_manager');
+DELETE FROM client_scope WHERE owner_id = (SELECT id from client_details where client_id = 'patient_data_manager');
+DELETE FROM client_details WHERE client_id = 'patient_data_manager';
+
+COMMIT;
+
+SET AUTOCOMMIT = 1;
