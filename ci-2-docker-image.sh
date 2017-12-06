c38a74d39e9d5b91a6fd446c8ac1d4ab1f337aa6
Eric Lineback <eric@interopion.com>
2017-12-06 14:29:58 -0700

    Update pipeline part 4

--- ci-2-docker-image.sh
+++ ci-2-docker-image.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "starting ci-2-docker-image.sh..."
-
+echo $IMAGE_NAME
 if [ $DOCKER_PUSH = "true" ]
 then
     export IMAGE_NAME=$(cat container-definitions_test.json | jq --raw-output '.[0].image')
+    echo $IMAGE_NAME
     docker login -u $NEXUS_USR -p $NEXUS_PWD nexus.hspconsortium.org:18083
     docker build -t $IMAGE_NAME .
 
