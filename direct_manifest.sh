dafb03dcb52ef0e7225be491c87186eb9840cc38
jacobC12 <jacob@interopion.com>
2018-02-28 15:39:44 -0700

    changed ACTIVE_ENV to TARGET_ENV

--- direct_manifest.sh
+++ direct_manifest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 #cat /src/.well-known/smart/manifest.json
-echo $ACTIVE_ENV
-if [ "$ACTIVE_ENV" = "prod" ]; then
+echo $TARGET_ENV
+if [ "$TARGET_ENV" = "prod" ]; then
     rm /src/.well-known/smart/manifest.json
     mv /src/.well-known/smart/manifest.prod.json /src/.well-known/smart/manifest.json
-elif [ "$ACTIVE_ENV" = "test" ]; then
+elif [ "$TARGET_ENV" = "test" ]; then
     rm /src/.well-known/smart/manifest.json
     mv /src/.well-known/smart/manifest.test.json /src/.well-known/smart/manifest.json
 fi
--- run_local_docker.sh
+++ run_local_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --build-arg ACTIVE_ENV=local -t patient-data-manager/node-web-app .
+docker build --build-arg TARGET_ENV=local -t patient-data-manager/node-web-app .
 docker run -p 8096:8096 patient-data-manager/node-web-app
