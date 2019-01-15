ef1633f0129fe6b75dfe3e3d48644ea9b199372d
d.dimitrov <dimitar@interopion.com>
2019-01-15 10:32:14 +0200

    Create value codeable concept as it should when under observation.

--- src/js/services.js
+++ src/js/services.js
@@ -21,9 +21,9 @@ angular.module('pdmApp.services', []).factory('$terminology', function ($http) {
 
     // Any function returning a promise object can be used to load values asynchronously
     terminologyService.getValueSetExpansion = function(val, min, url) {
-        if(expansionNotSupported.indexOf(url) !== -1) return;
+        if(expansionNotSupported.indexOf(url) !== -1 || !url) return;
         var deferred = $.Deferred();
-        var lookupUrl = url.substr(19)
+        var lookupUrl = url.substr(19);
         //Example call
         // https://api-v5-stu3.hspconsortium.org/stu3/open/ValueSet/observation-codes/$expand?filter=100
         var path = lookupUrl + '/$expand?filter=' + val;
